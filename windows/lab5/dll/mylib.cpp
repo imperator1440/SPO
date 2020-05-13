d7a5e6136a97706bcec50f68053aca05a4462578
imperator1440 <61932449+imperator1440@users.noreply.github.com>
2020-05-13 13:06:14 +0300

    Update mylib.cpp

--- windows/lab5/dll/mylib.cpp
+++ windows/lab5/dll/mylib.cpp
@@ -12,7 +12,6 @@ BOOL ReadTestFile(const char* filePath, const char* message)
 	overlappedStructure.hEvent = NULL;
 	HANDLE readFile = CreateFileA(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
 	ReadFile(readFile, (LPVOID)message, 80, NULL, &overlappedStructure);
-	WaitForSingleObject(readFile, INFINITE);
 	CloseHandle(readFile);
 
 	std::cout << "Read file - finish" << std::endl;
@@ -29,7 +28,6 @@ BOOL WriteTestFile(const char* filePath, const char* message, unsigned int size,
 	overlappedStructure.hEvent = NULL;
 	HANDLE writeFile = CreateFileA(filePath, GENERIC_WRITE, FILE_SHARE_WRITE, NULL, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
 	WriteFile(writeFile, message, size, NULL, &overlappedStructure);
-	WaitForSingleObject(writeFile, INFINITE);
 	CloseHandle(writeFile);
 
 	std::cout << "Write file - finish" << std::endl;
